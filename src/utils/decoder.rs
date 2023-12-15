2375ad9e3e21ca003c38759ccd2c4fdf92e10b7c
Vann-Dev <stevanvincent05@gmail.com>
2023-12-16 01:26:26 +0700

    chore: remove comments

--- src/utils/decoder.rs
+++ src/utils/decoder.rs
@@ -6,10 +6,8 @@ use std::env;
 pub fn decode(value: String) -> String {
     dotenv().ok();
 
-    // decode hex to base64 first
     let hex = hex::decode(value).unwrap();
 
-    // base64 decode to get real value
     let mc = new_magic_crypt!(env::var("key").unwrap(), 256);
     let result = mc
         .decrypt_base64_to_string(String::from_utf8(hex.clone()).unwrap())
@@ -21,13 +19,11 @@ pub fn decode(value: String) -> String {
 pub fn validate(value: String) -> bool {
     dotenv().ok();
 
-    // decode hex to base64 first
     let hex = match hex::decode(value) {
         Ok(val) => val,
         Err(_) => return false,
     };
 
-    // base64 decode to get real value
     let mc = new_magic_crypt!(env::var("key").unwrap(), 256);
 
     let result = match String::from_utf8(hex.clone()) {
