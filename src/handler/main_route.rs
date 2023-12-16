e137c46f619de0d37873ee3a9a441c5440b0c732
Vann-Dev <stevanvincent05@gmail.com>
2023-12-16 08:45:12 +0700

    fix: image not showed

--- src/handler/main_route.rs
+++ src/handler/main_route.rs
@@ -4,7 +4,7 @@ use crate::utils::fetch::fetch;
 pub async fn main_route(params: String) -> Result<impl warp::Reply, warp::Rejection> {
     if validate(params.clone()) {
         let real_uri = decode(params);
-        let result = fetch(real_uri).await.unwrap().into_raw();
+        let result = fetch(real_uri).await.unwrap().to_vec();
 
         Ok(warp::reply::with_header(
             result,
--- src/utils/fetch.rs
+++ src/utils/fetch.rs
@@ -1,11 +1,22 @@
-use image::{ImageBuffer, Rgb};
+use std::io::Cursor;
 
-pub async fn fetch(
-    uri: String,
-) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, Box<dyn std::error::Error>> {
+pub async fn fetch(uri: String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let resp = reqwest::get(uri).await?.bytes().await?;
 
-    let image = image::load_from_memory(&resp).unwrap().into_rgb8();
+    let image = image::load_from_memory(&resp).unwrap().to_rgb8();
+    let (width, height) = (image.clone().width(), image.clone().height());
+    let image_byte_vec = image.clone().into_raw();
 
-    Ok(image)
+    let mut result = Vec::new();
+    image::write_buffer_with_format(
+        &mut Cursor::new(&mut result),
+        &image_byte_vec,
+        width,
+        height,
+        image::ColorType::Rgb8,
+        image::ImageFormat::Jpeg,
+    )
+    .unwrap();
+
+    Ok(result)
 }
