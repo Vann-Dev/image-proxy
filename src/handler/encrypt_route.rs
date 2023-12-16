2650ae7762fed11eb41d33d7334a8b4439e208d1
Vann-Dev <stevanvincent05@gmail.com>
2023-12-16 08:56:49 +0700

    ref: change response data

--- src/handler/encrypt_route.rs
+++ src/handler/encrypt_route.rs
@@ -1,4 +1,5 @@
 use crate::utils::encrypter::encrypt;
+use serde_json::json;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 
@@ -9,5 +10,5 @@ pub struct RequestStruct {
 pub async fn encrypt_route(request: RequestStruct) -> Result<impl warp::Reply, warp::Rejection> {
     let encrypt_url = encrypt(request.url);
 
-    Ok(warp::reply::json(&encrypt_url))
+    Ok(warp::reply::json(&json!({"data": encrypt_url})))
 }
